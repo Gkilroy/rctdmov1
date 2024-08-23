91cbe740217668c19faab7031002b92f2cb75a6e
Gkilroy <Griffin.Kilroy@Gmail.com>
2024-08-23 10:10:48 -0700

    update app and appindex js files

--- rctdmo-v1/src/App.js
+++ rctdmo-v1/src/App.js
@@ -1,11 +1,31 @@
 import './App.css';
+import './AppIndex.css';
+import NavMenu from './components/NavMenu';
+//-- github pages webhost - router 
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+//-- localhost or other webhost - router 
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home';
+import ContactUs from './components/pages/ContactUs';
 
 function App() {
   return (
-    <div>
-<p> Demo React App v2 2.0.0</p>
-    </div>
+    <Router>
+      <>
+        <NavMenu/>
+        <div className='container'>        
+          <Routes>
+            <Route path="/" element={<Home title="Demo Todo List App - 1.0.9" />}/>
+            <Route path='/contact' element={<ContactUs/>}/>
+            <Route path="*" element={
+                <>
+                  <h4>No page found</h4>
+                </>
+            }/>
+          </Routes>
+        </div>
+      </>
+    </Router>
   );
 }
 
